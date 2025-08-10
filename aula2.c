b8cda8c6c91de5c6a170934fd4aa26bf18f8f3b4
Alexandre-Milim <alexandremilim15@gmail.com>
2025-08-09 23:09:31 -0300

    Introduction C

---
+++ aula2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <locale.h>
+int main(){
+setlocale (LC_ALL, "Portuguese");
+int contador;
+float nota;
+contador = 10;
+nota = 9.5;
+printf("O valor da variavel contador é %d", contador);
+printf("\nO valor da variavel nota é %.2f", nota);
+
+return 0;
+}
